833d75eab6e7a04ed2703091f0b1494a73892fd1
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-04-21 15:30:44 +0200

    Responsive Navbar :100:

--- style.js
+++ style.js
@@ -1,7 +1,29 @@
+
 function openNavbar() {
     document.getElementById("hamburgerNavbarLinks").style.width = "100%";
+    document.getElementById("closeButoon").style.display = "flex";
+    document.getElementById("hamburgerNavbarLinks").style.display = "flex";
+    document.querySelector("main").classList.toggle("active");
+  
+
 }
+
+var x = window.matchMedia("(max-width: 540px)")
 
 function closeNavbar() {
     document.getElementById("hamburgerNavbarLinks").style.width = "0%";
+    document.getElementById("closeButoon").style.display = "none";
+    document.getElementById("hamburgerButton").style.display = "none"; 
+    document.querySelector("main").classList.toggle("active");
+
+    
+    if (x.matches) {
+        document.getElementById("hamburgerButton").style.display = "flex";
+        
+        
+    }
+    else {
+        document.getElementById("hamburgerButton").style.display = "none";
+    }
+
 }
