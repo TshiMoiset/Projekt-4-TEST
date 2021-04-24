281842af6e5788fe62e7cf9cba7fcfa1ec0a9df4
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-04-25 01:06:13 +0200

    Update style.js

--- style.js
+++ style.js
@@ -2,14 +2,14 @@ let screenSizex = window.matchMedia("(max-width: 768px)")
 let screenSizey = window.matchMedia("(max-width: 568px)")
 
 function openNavbar() {
-    document.getElementById("closeButoon").style.display = "flex";
-    document.getElementById("hamburgerNavbarLinks").style.display = "flex";
-    document.querySelector("body").classList.toggle("active");
+    document.getElementById("closeButoon").style.display = "flex";  // Knappen crossbutton kommer att visas.
+    document.getElementById("hamburgerNavbarLinks").style.display = "flex";     // Innehållet i hamburgerNavbarLinks visas.
+    document.querySelector("body").classList.toggle("active");  // Koden gör så att amn inte kan skrolla när innehållet i hamburgerNavbarLinks visas
 
-    if (screenSizey.matches) {
-        document.getElementById("hamburgerNavbarLinks").style.width = "100%";
+    if (screenSizey.matches) {  // Om skärmstorleken är 568px tar hamburgerNavbarLinks 100% av skärmen
+        document.getElementById("hamburgerNavbarLinks").style.width = "100%";  
     } else if (screenSizex.matches) {
-        document.getElementById("hamburgerNavbarLinks").style.width = "60%";
+        document.getElementById("hamburgerNavbarLinks").style.width = "60%";    // Om skärmstorleken är 768px tar hamburgerNavbarLinks 60% av skärmen
         document.querySelector("main").style.opacity = "0.4";
         document.querySelector("main").style.backgroundColor = "rgba(0,0,0,0.4)"
     }
