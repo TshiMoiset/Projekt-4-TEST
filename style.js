2637d8c45e79f6c0fb06c5f9cb5d7d7add20aea3
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-04-24 22:29:09 +0200

    768px By: Jing

--- style.js
+++ style.js
@@ -1,9 +1,18 @@
+let y = window.matchMedia("(max-width: 768px)")
+let y2 = window.matchMedia("(max-width: 568px)")
 
 function openNavbar() {
-    document.getElementById("hamburgerNavbarLinks").style.width = "100%";
     document.getElementById("closeButoon").style.display = "flex";
     document.getElementById("hamburgerNavbarLinks").style.display = "flex";
     document.querySelector("body").classList.toggle("active");
+
+    if (y2.matches) {
+        document.getElementById("hamburgerNavbarLinks").style.width = "100%";
+    } else if (y.matches) {
+        document.getElementById("hamburgerNavbarLinks").style.width = "60%";
+        document.querySelector("main").style.opacity = "0.4";
+        document.querySelector("main").style.backgroundColor = "rgba(0,0,0,0.4)"
+    }
 }
 
 let x = window.matchMedia("(max-width: 768px)")
@@ -13,6 +22,9 @@ function closeNavbar() {
     document.getElementById("closeButoon").style.display = "none";
     document.getElementById("hamburgerButton").style.display = "none"; 
     document.querySelector("body").classList.toggle("active");
+
+    document.querySelector("main").style.opacity = "1";
+    document.querySelector("main").style.backgroundColor = "rgba(0,0,0,0)";
     
     if (x.matches) {
         document.getElementById("hamburgerButton").style.display = "flex";
