615f7d0dd0be549ffe46ee73498c247e50476e7e
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-04-24 16:09:08 +0200

    Scroll

--- style.js
+++ style.js
@@ -3,7 +3,7 @@ function openNavbar() {
     document.getElementById("hamburgerNavbarLinks").style.width = "100%";
     document.getElementById("closeButoon").style.display = "flex";
     document.getElementById("hamburgerNavbarLinks").style.display = "flex";
-    document.querySelector("main").classList.toggle("active");
+    document.querySelector("body").classList.toggle("active");
 }
 
 let x = window.matchMedia("(max-width: 768px)")
@@ -12,7 +12,7 @@ function closeNavbar() {
     document.getElementById("hamburgerNavbarLinks").style.width = "0%";
     document.getElementById("closeButoon").style.display = "none";
     document.getElementById("hamburgerButton").style.display = "none"; 
-    document.querySelector("main").classList.toggle("active");
+    document.querySelector("body").classList.toggle("active");
     
     if (x.matches) {
         document.getElementById("hamburgerButton").style.display = "flex";
